a0e2978e82f536ec645e91db17c340e78f5a1776
Piyush Majumdar <107701542+libertorex@users.noreply.github.com>
2023-07-12 22:22:32 +0530

    Update ErrorHandling.sol

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.13;
 
 contract Error {
     function testRequire(uint _i) public pure {
-        require(_i > 10, "Input must be greater than 20");
+        require(_i > 10, "Input must be greater than 10");
     }
 
     function testRevert(uint _i) public pure {
         if (_i <= 10) {
-            revert("Input must be greater than 20");
+            revert("Input must be greater than 10");
         }
     }
 
